ea04cf8e1feed5840707533324aa1203ebe2d139
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-07-07 15:36:08 +0545

    deployment bug fix

--- dist/data/bookData.js
+++ dist/data/bookData.js
@@ -44,67 +44,59 @@ const books = {
         health_nepali: "https://moecdc.gov.np/storage/gallery/1682061955.pdf"
     },
     "6": {
-        nepali: "",
-        english: "",
-        math_nepali: "https://moecdc.gov.np/storage/gallery/1672799879.pdf",
+        nepali: "https://moecdc.gov.np/storage/gallery/1751285383.pdf",
+        english: "https://moecdc.gov.np/storage/gallery/1751285293.pdf",
+        math_nepali: "https://moecdc.gov.np/storage/gallery/1726821982.pdf",
         math_english: "https://moecdc.gov.np/storage/gallery/1708925838.pdf",
-        science_nepali: "https://moecdc.gov.np/storage/gallery/1672799965.pdf",
+        science_nepali: "https://moecdc.gov.np/storage/gallery/1726822016.pdf",
         science_english: "https://moecdc.gov.np/storage/gallery/1709542968.pdf",
-        social: "https://moecdc.gov.np/storage/gallery/1672800056.pdf",
+        social: "https://moecdc.gov.np/storage/gallery/1726822058.pdf",
         health_physical_nepali: "https://moecdc.gov.np/storage/gallery/1710760807.pdf",
         health_physical_english: "https://moecdc.gov.np/storage/gallery/1709191880.pdf"
     },
     "7": {
-        nepali: "https://moecdc.gov.np/storage/gallery/1672800491.pdf",
+        nepali: "https://moecdc.gov.np/storage/gallery/1726822115.pdf",
         english: "https://moecdc.gov.np/storage/gallery/1672800534.pdf",
-        math_nepali: "https://moecdc.gov.np/storage/gallery/1672800575.pdf",
-        math_english: "https://moecdc.gov.np/storage/gallery/1709112299.pdf",
+        math_nepali: "https://moecdc.gov.np/storage/gallery/1726822267.pdf",
+        math_english: "https://moecdc.gov.np/storage/gallery/1730268331.pdf",
         science_nepali: "https://moecdc.gov.np/storage/gallery/1674294172.pdf",
         science_english: "https://moecdc.gov.np/storage/gallery/1709186060.pdf",
-        social: "https://moecdc.gov.np/storage/gallery/1672800794.pdf",
-        health_physical_nepali: "https://moecdc.gov.np/storage/gallery/1672800992.pdf",
-        health_physical_english: "https://moecdc.gov.np/storage/gallery/1708934002.pdf"
+        social: "https://moecdc.gov.np/storage/gallery/1726822399.pdf",
+        health_physical_nepali: "https://moecdc.gov.np/storage/gallery/1726822587.pdf",
+        health_physical_english: "https://moecdc.gov.np/storage/gallery/1726822587.pdf"
     },
     "8": {
-        nepali: "https://moecdc.gov.np/storage/gallery/1682234494.pdf",
+        nepali: "https://moecdc.gov.np/storage/gallery/1751285144.pdf",
         english: "https://moecdc.gov.np/storage/gallery/1689238602.pdf",
         math_nepali: "https://moecdc.gov.np/storage/gallery/1684297112.pdf",
-        math_english: "https://moecdc.gov.np/storage/gallery/1692693895.pdf",
-        science_nepali: [
-            "https://moecdc.gov.np/storage/gallery/1684481476.pdf",
-            "https://moecdc.gov.np/storage/gallery/1684483448.pdf"
-        ],
+        math_english: "https://moecdc.gov.np/storage/gallery/1730105209.pdf",
+        science_nepali: "https://moecdc.gov.np/storage/gallery/1684481476.pdf",
         science_english: "https://moecdc.gov.np/storage/gallery/1687157819.pdf",
         social: "https://moecdc.gov.np/storage/gallery/1682060895.pdf",
         health_physical_nepali: "https://moecdc.gov.np/storage/gallery/1708859361.pdf",
         health_physical_english: "https://moecdc.gov.np/storage/gallery/1687151177.pdf"
     },
     "9": {
-        nepali: "https://moecdc.gov.np/storage/gallery/1672903464.pdf",
+        nepali: "https://moecdc.gov.np/storage/gallery/1746444756.pdf",
         english: "https://moecdc.gov.np/storage/gallery/1688289325.pdf",
-        math_nepali: "https://moecdc.gov.np/storage/gallery/1672910414.pdf",
-        math_english: "https://moecdc.gov.np/storage/gallery/1708861009.pdf",
+        math_nepali: "https://moecdc.gov.np/storage/gallery/1737352895.pdf",
+        math_english: "https://moecdc.gov.np/storage/gallery/1730785418.pdf",
         science_nepali: "https://moecdc.gov.np/storage/gallery/1672910467.pdf",
-        science_english: "https://moecdc.gov.np/storage/gallery/1709189449.pdf",
-        social: "https://moecdc.gov.np/storage/gallery/1672910576.pdf"
+        science_english: "https://moecdc.gov.np/storage/gallery/1726655547.pdf",
+        social: "https://moecdc.gov.np/storage/gallery/1672910576.pdf",
+        office_management_and_account: "https://moecdc.gov.np/storage/gallery/1748936723.pdf",
+        computer_science: "https://moecdc.gov.np/storage/gallery/1736920682.pdf",
     },
     "10": {
-        nepali: "https://moecdc.gov.np/storage/gallery/1681370216.pdf",
-        computer_science: "http://lib.moecdc.gov.np/elibrary/pages/download.php?direct=1&noattach=true&ref=3667&ext=pdf",
+        nepali: "https://moecdc.gov.np/storage/gallery/1746444352.pdf",
         english: "https://moecdc.gov.np/storage/gallery/1681716079.pdf",
-        math_nepali: "https://moecdc.gov.np/storage/gallery/1681715186.pdf",
-        math_english: "https://moecdc.gov.np/storage/gallery/1687066510.pdf",
-        science_nepali: [
-            "https://moecdc.gov.np/storage/gallery/1682668847.pdf",
-            "https://moecdc.gov.np/storage/gallery/1682668904.pdf"
-        ],
-        science_english: [
-            "https://moecdc.gov.np/storage/gallery/1692097269.pdf",
-            "https://moecdc.gov.np/storage/gallery/1687942336.pdf",
-            "https://moecdc.gov.np/storage/gallery/1688286170.pdf"
-        ],
-        social: "https://moecdc.gov.np/storage/gallery/1710845068.pdf",
-        optional_math: "http://lib.moecdc.gov.np/elibrary/pages/download.php?direct=1&noattach=true&ref=3883&ext=pdf"
+        math_nepali: "https://moecdc.gov.np/storage/gallery/1737352972.pdf",
+        math_english: "https://moecdc.gov.np/storage/gallery/1730786225.pdf",
+        science_nepali: "https://moecdc.gov.np/storage/gallery/1726588881.pdf",
+        science_english: "https://moecdc.gov.np/storage/gallery/1730103584.pdf",
+        social: "https://moecdc.gov.np/storage/gallery/1747732660.pdf",
+        optional_math: "https://moecdc.gov.np/storage/gallery/1751018986.pdf",
+        office_management_and_account: "https://moecdc.gov.np/storage/gallery/1748936763.pdf",
     }
 };
 exports.default = books;
